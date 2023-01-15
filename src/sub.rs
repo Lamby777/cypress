bc1c1b952dfdbc940e3c757b404ee2ddb97206fc
Dex <sheepareawesome777@gmail.com>
2023-01-15 04:18:05 -0500

    split out "for each" logic into its own fn

--- src/sub.rs
+++ src/sub.rs
@@ -16,33 +16,47 @@ pub fn init() {
 }
 
 pub fn passwd(args: &[String]) {
-	let users = all_account_users();
-
 	let em =	args.len()	== 2	&&
 					args[0]		== "em";
 	
 	let new_pass = args.last().unwrap();
-	let current_uid = get_current_uid();
 
-	let mut errors = 0u16;
-
-	for user in users {
-		if em && (user.uid() == current_uid) { continue };
-
+	let errors = for_each_user(|user| {
 		let name = user.name().to_string_lossy();
 
 		let cmd = format!("sudo passwd {} <<< \"{}\"$'\n'\"{}\"", name, new_pass, new_pass);
 
-		if bash!(cmd).is_err() {
-			errors += 1;
-		}
-	}
+		bash!(cmd).is_ok()
+	}, if em { Some(get_current_uid()) } else { None } );
 
 	if errors > 0 {
 		println!("{} passwords had issues being updated...", errors);
 	} else {
 		println!("Passwords updated successfully!");
 	}
+}
+
+// returns number of fails
+fn for_each_user<C>(action: C, except: Option<u32>) -> u16
+	where C: Fn(User) -> bool {
+
+	let users = all_account_users();
+	let excepting = except.is_some();
+
+	// don't make this call if there isn't a point anyway
+	let current_uid = if excepting { get_current_uid() } else { 0 };
+
+	let mut errors = 0;
+
+	for user in users {
+		if excepting && (user.uid() == current_uid) { continue };
+
+		if !action(user) {
+			errors += 1;
+		}
+	}
+
+	errors
 }
 
 fn all_account_users() -> impl Iterator<Item = User> {
