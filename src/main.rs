466f9e34c5f3632b9e18d25a838041442f7bf3ff
Dex <sheepareawesome777@gmail.com>
2023-01-20 04:12:22 -0500

    Changed package name to `pz`

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Pass args stuff into lib.rs
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	pizza::main(std::env::args().collect::<Vec<String>>())
+	pz::main(std::env::args().collect::<Vec<String>>())
 }
