b51906e0fb7539e1fc0770a4bf6556d2f9906749
Dex <sheepareawesome777@gmail.com>
2023-01-20 04:18:09 -0500

    rm bum + minor help txt changes

--- src/sh/init.sh
+++ src/sh/init.sh
@@ -8,7 +8,7 @@ sudo apt update
 sudo apt -y upgrade
 
 # Install all these packages, whether it be for points, or for more efficient patching
-sudo apt -y install python3 ufw gufw aptitude mlocate bum
+sudo apt -y install python3 ufw gufw aptitude mlocate
 
 # These are on separate lines to be easily commented out
 sudo apt -y install fail2ban
