0bbaacf8c297065afa9fa60b5e87e934df04bb51
Cherry <cherry@sparklet.org>
2023-10-09 04:37:58 -0400

    debugging curses and char codes

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const curses = @import("curses.zig");
 const heap = std.heap;
 const fmt = std.fmt;
+const mem = std.mem;
+const Allocator = mem.Allocator;
 
 pub fn main() !void {
     var gpa = heap.GeneralPurposeAllocator(.{}){};
@@ -12,7 +14,7 @@ pub fn main() !void {
         }
     }
 
-    const ally = gpa.allocator();
+    var ally = gpa.allocator();
 
     // Initialize the curses library
     const win = try curses.initscr(ally);
@@ -29,14 +31,31 @@ pub fn main() !void {
         try win.mvaddstr(4, 2, "q to quit");
         try win.boxme();
 
-        switch (try win.getch()) {
-            'q' => {
-                break;
-            },
-
-            else => {},
-        }
+        const cmdEntered = try getCmd(&ally, &win);
+        _ = cmdEntered;
     }
 
     _ = try curses.endwin();
+}
+
+fn intToStr(ally: *Allocator, i: i32) ![]const u8 {
+    return fmt.allocPrint(ally.*, "{d}", .{i});
+}
+
+fn getCmd(ally: *Allocator, win: *const curses.Window) !void {
+    var cursorPos: u8 = 0;
+    var cmd: [1024]u8 = undefined;
+    _ = cmd;
+
+    while (true) {
+        const key = try win.getch();
+
+        if (key == 127) {
+            try win.mvaddch(40, cursorPos, 'a');
+        }
+
+        try win.mvaddch(20, cursorPos, @intCast(key));
+        try win.mvaddstr(21, 3, try intToStr(ally, key));
+        cursorPos += 1;
+    }
 }
