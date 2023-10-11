52b3b60177d62e2076ebb766f800a1ec0df32b9c
Cherry <cherry@sparklet.org>
2023-10-11 15:26:48 -0400

    fix spacing

--- src/main.zig
+++ src/main.zig
@@ -79,7 +79,7 @@ pub fn main() !void {
 
 fn printCmdLine(cmd: []u8, offset: u8) !void {
     var lineno = cs.CMD_LINENO + offset;
-    try win.mvaddch(lineno + 1, 2, cs.CMD_PROMPT);
+    try win.mvaddch(lineno, 2, cs.CMD_PROMPT);
     try win.mvaddstr(lineno, 4, cmd);
 }
 
@@ -98,8 +98,8 @@ fn getCmd(ally: *Allocator, lineOffset: u8) !CommandBuffer {
         // write cmd buffer to prompt
         try drawWin(ally);
         try win.mvaddch(lineno + 1, 2, cs.CMD_PROMPT);
-        try win.mvaddstr(lineno, 4, cmd[0..cursorPos]);
-        try curses.move(lineno, 4 + cursorPos);
+        try win.mvaddstr(lineno + 1, 4, cmd[0..cursorPos]);
+        try curses.move(lineno + 1, 4 + cursorPos);
 
         const key = try win.getch();
         const ch: u32 = @intCast(key);
