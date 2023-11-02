bb389e4811a66b8987b1fb0a6e42d65b877fc902
Cherry <cherry@sparklet.org>
2023-11-01 21:33:07 -0400

    rm ubuntu/fedora picker, script just runs on ubuntu

--- src/sh/init.sh
+++ src/sh/init.sh
@@ -17,55 +17,35 @@ grep -qF 'multi on' && sed 's/multi/nospoof/' || echo 'nospoof on' >> /etc/host.
 # Login configs
 sed -i 's/PASS_MAX_DAYS.*$/PASS_MAX_DAYS 90/;s/PASS_MIN_DAYS.*$/PASS_MIN_DAYS 10/;s/PASS_WARN_AGE.*$/PASS_WARN_AGE 7/' /etc/login.defs
 
+# Update all packages
+apt update
+apt -y upgrade
 
-echo "Which distro are you using?"
-select yn in "Yes" "No"; do
-	case $yn in
-		Ubuntu ) ubuntuFn;;
-		Fedora ) fedoraFn;;
-	esac
-done
+# install neovim (do chezmoi manually with the other script)
+wget -O /usr/local/bin/nvim https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x /usr/local/bin/nvim
 
-function ubuntuFn {
-	# Update all packages
-	apt update
-	apt -y upgrade
+# Install all these packages, whether it be for points, or for more efficient patching
+apt -y install python3 ufw gufw aptitude mlocate
 
-  # install neovim (do chezmoi manually with the other script)
-  wget -O /usr/local/bin/nvim https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-  chmod u+x /usr/local/bin/nvim
+# These are on separate lines to be easily commented out
+apt -y install fail2ban
 
-	# Install all these packages, whether it be for points, or for more efficient patching
-	apt -y install python3 ufw gufw aptitude mlocate
+# echo 'auth required pam_tally2.so deny=5 onerr=fail unlock_time=1800' >> /etc/pam.d/common-auth
+# apt -y install libpam-cracklib
+# sed -i 's/\(pam_unix\.so.*\)$/\1 remember=5 minlen=8/' /etc/pam.d/common-password
+# sed -i 's/\(pam_cracklib\.so.*\)$/\1 ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1/' /etc/pam.d/common-password
 
-	# These are on separate lines to be easily commented out
-	apt -y install fail2ban
+apt-get -y install auditd && auditctl -e 1
+apt-get -y install clamtk && freshclam
 
-	# echo 'auth required pam_tally2.so deny=5 onerr=fail unlock_time=1800' >> /etc/pam.d/common-auth
-	# apt -y install libpam-cracklib
-	# sed -i 's/\(pam_unix\.so.*\)$/\1 remember=5 minlen=8/' /etc/pam.d/common-password
-	# sed -i 's/\(pam_cracklib\.so.*\)$/\1 ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1/' /etc/pam.d/common-password
+# Disable USB/Firewire/Thunderbolt
+echo 'install usb-storage /bin/true' >> /etc/modprobe.d/disable-usb-storage.conf
+echo "blacklist firewire-core" >> /etc/modprobe.d/firewire.conf
+echo "blacklist thunderbolt" >> /etc/modprobe.d/thunderbolt.conf
 
-	apt-get -y install auditd && auditctl -e 1
-	apt-get -y install clamtk && freshclam
-
-	# Disable USB/Firewire/Thunderbolt
-	echo 'install usb-storage /bin/true' >> /etc/modprobe.d/disable-usb-storage.conf
-	echo "blacklist firewire-core" >> /etc/modprobe.d/firewire.conf
-	echo "blacklist thunderbolt" >> /etc/modprobe.d/thunderbolt.conf
-
-	# Detect rootkits
-	apt-get -y install chkrootkit rkhunter
-	chkrootkit
-	rkhunter --update
-	rkhunter --check
-}
-
-function fedoraFn {
-	# Install UFW
-	dnf install ufw
-	systemctl stop firewalld
-	systemctl disable firewalld
-	systemctl enable ufwd
-	systemctl start ufwd
-}
+# Detect rootkits
+apt-get -y install chkrootkit rkhunter
+chkrootkit
+rkhunter --update
+rkhunter --check
