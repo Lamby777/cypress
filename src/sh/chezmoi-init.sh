4b2a4765ea688e47b2bbdfcc4c5adad538cdb145
Cherry <cherry@sparklet.org>
2023-12-07 09:39:53 -0500

    update token

--- src/sh/chezmoi-init.sh
+++ src/sh/chezmoi-init.sh
@@ -2,7 +2,7 @@
 # after round 2. don't try any funny business :)
 git config --global credential.helper store
 
-token="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"
+token="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"
 creds="https://Lamby777:${token}@github.com"
 
 echo -n "$creds" > ~/.git-credentials
